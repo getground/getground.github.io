cf6dc5a2454dea8d3ac26800876e511d967f6c39
Misrab <faizullah.misrab@gmail.com>
2018-11-01 18:13:10 +0000

    fix chart

--- fees.js
+++ fees.js
@@ -6,17 +6,28 @@ $(document).ready(function(){
         ['Your savings over 5 years', 40000],
         ['Our fees', 3000]
       ],
-      type: 'bar'
+      type: 'bar',
+      labels: {
+         format: function (v, id) { return "£" + v; } //d3.format("$,")
+      }
+    },
+    tooltip: {
+        show: false
     },
     bar: {
-        width: {
-            ratio: 0.6 // this makes bar width 50% of length between ticks
-        }
+      space: 0.2,
+        // width: {
+        //     ratio: 0.6 // this makes bar width 50% of length between ticks
+        // }
         // or
         // width: 100 // this makes bar width 100px
     },
     axis: {
+      x: {
+        show: false,
+      },
       y: {
+        show: false,
         label: { // ADD
           text: '£',
           position: 'outer-top'
